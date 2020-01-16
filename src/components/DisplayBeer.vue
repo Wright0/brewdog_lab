bac19d8fbfed280c501bdff3b1371e9483b6a490
Olivia <olivia.wright101@gmail.com>
2020-01-16 13:45:58 +0000

    Adding new displayBeer component

---
+++ src/components/DisplayBeer.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+<p>Hallooooooo</p>
+</template>
+
+<script>
+export default {
+  name: 'display-beer',
+  props: ['beerObject']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
